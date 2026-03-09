27f9b4c15723bfd4fa37b66906d02500e010d47c
Farhat Samara <farhatsamara@gmail.com>
2026-03-09 15:28:59 -0500

    Added react project

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,13 @@
+import { UiComponent } from "~/ui/ui-component";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Plugin Test" },
+    { name: "description", content: "Delay Plugin" },
+  ];
+}
+
+export default function Home() {
+  return <UiComponent />;
+}
---
+++ frontend/app/ui/ui-component.tsx
@@ -0,0 +1,6 @@
+
+export function UiComponent() {
+    return (
+        <span>Main component</span>
+    )
+}
